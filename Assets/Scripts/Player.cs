a9647ede1a6340331cbde5e5ad4cd65c63e525a4
Hieu <you@example.com>
2023-10-20 03:37:11 +0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float RollTime;
+    bool rollOnce = false;
+
+    private float rollBoost = 2f;
+    private float rollTime;
+    private Rigidbody rb;
+
+    public SpriteRenderer characterSR;
+    public Animator animator;
+    public Vector3 moveInput;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Roll();
+    }
+
+    void Move()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        if (moveInput.x * moveInput.y != 0)
+        {
+            // Toc do di theo duong cheo
+            transform.position += moveInput * Time.deltaTime * moveSpeed / Mathf.Sqrt(2);
+        }
+        else
+        {
+            // Toc do di theo duong thang
+            transform.position += moveInput * Time.deltaTime * moveSpeed;
+        }
+
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if (moveInput.x != 0)
+        {
+            if (moveInput.x > 0)
+                characterSR.transform.localScale = new Vector3(1, 1, 0);
+            else
+                characterSR.transform.localScale = new Vector3(-1, 1, 0);
+        }
+    }
+
+    void Roll()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && rollOnce == false)
+        {
+            moveSpeed += rollBoost;
+            rollTime = RollTime;
+            rollOnce = true;
+
+            animator.SetBool("Roll", true);
+        }
+
+        if (rollTime <= 0 && rollOnce == true)
+        {
+            moveSpeed -= rollBoost;
+            rollOnce = false;
+
+            animator.SetBool("Roll", false);
+        }
+        else
+        {
+            rollTime -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePos;
+    public GameObject muzzle;
+    public float TimeBtwFire;
+    public float bulletForce;
+
+    private float timeBtwFire;
+
+    void Update()
+    {
+        RotateGun();
+        timeBtwFire -= Time.deltaTime;
+        if (Input.GetMouseButton(0) && timeBtwFire < 0)
+        {
+            FireBullet();
+        }
+    }
+
+    void RotateGun()
+    {
+        // Xac dinh vi tri con tro chuot
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - transform.position;
+        // Xac dinh goc xoay sung
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = rotation;
+        // Doi chieu sung khi xoay
+        if(transform.eulerAngles.z > 90 && transform.eulerAngles.z < 270)
+        {
+            transform.localScale = new Vector3(1, -1, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 0);
+        }
+    }
+
+    void FireBullet()
+    {
+        timeBtwFire = TimeBtwFire;
+
+        GameObject bullerTmp = Instantiate(bullet, firePos.position, Quaternion.identity);
+        // Hieu ung
+        Instantiate(muzzle, firePos.position, transform.rotation, transform);
+        Rigidbody2D rb = bullerTmp.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * bulletForce, ForceMode2D.Impulse);
+    }
+}
